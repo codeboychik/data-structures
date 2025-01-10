9e9320a6450ab954af3238c522718b798ce6ee32
andrey.chernenko <andrey.chernenko@ibacz.cz>
2025-01-10 10:11:28 +0100

    ReverseInteger solved

--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -4,7 +4,7 @@ public class ReverseInteger {
         boolean x_negative = x < 0;
         x = Math.abs(x);
         for(int i = 10; x > 0;) {
-            if((x % i) * Math.pow(10, String.valueOf(x).length() - 1) > Integer.MAX_VALUE || (!x_negative && r < 0) || (x_negative && r > 0)) {
+            if(r + (int) ((x % i) * Math.pow(10, String.valueOf(x).length() - 1)) < 0) {
                 return 0;
             }
             r += (int) ((x % i) * Math.pow(10, String.valueOf(x).length() - 1));
@@ -14,9 +14,6 @@ public class ReverseInteger {
         return r;
     }
     public static void main(String[] args) {
-        System.out.println(reverse(-1563847412));
-        // 8_798_798_798
-
-
+        System.out.println(reverse(-1563847412)); // 1ms runtime
     }
 }
