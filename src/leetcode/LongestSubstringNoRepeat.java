8f7db555f2952501a9ac2a750af55f50e51b8dfb
Andrey Chernenko <xchernen@mendelu.cz>
2025-10-24 16:21:48 +0200

    Time: O(n) Space: O(min(n, m))

--- src/leetcode/LongestSubstringNoRepeat.java
+++ src/leetcode/LongestSubstringNoRepeat.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
 /**
  *
  * File is created by andreychernenko at 24.10.2025
@@ -14,30 +16,26 @@ public class LongestSubstringNoRepeat {
     *
     * */
     public static int lengthOfLongestSubstring(String s) {
-        //String head = s.substring(0,1);
-        StringBuilder worm = new StringBuilder();
-        int max = 0;
-        for(int i = 0; i < s.length();i++){
-            worm.append(s.charAt(i));
-            max = worm.length() > max ? max + 1: max;
-            for (int j = i + 1; j < s.length(); j++) {
-                String next = s.substring(j, j+1);
-                if (!worm.toString().contains(next)) {
-                    worm.append(next);
-                    max = worm.length() > max ? max + 1: max;
-                    if(s.length() - j == 1) {
-                        worm = new StringBuilder();
-                    }
-                } else {
-                    if (worm.length() > max) {
-                        System.out.println(worm);
-                        max = worm.length();
-                    }
-                    worm = new StringBuilder();
-                    break;
-                }
+        int left = 0;                // start of the window
+        int max = 0;                 // max substring length found
+        Map<Character, Integer> map = new HashMap<>();  // stores last seen index of each char
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+
+            // If char was seen before and is inside current window,
+            // move the left pointer right after its last seen position
+            if (map.containsKey(c) && map.get(c) >= left) {
+                left = map.get(c) + 1;
             }
+
+            // store or update last seen index of this char
+            map.put(c, right);
+
+            // update max length (current window = right - left + 1)
+            max = Math.max(max, right - left + 1);
         }
+
         return max;
     }
 
