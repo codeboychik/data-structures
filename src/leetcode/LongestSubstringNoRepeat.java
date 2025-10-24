c3dcf1cb233218b1c8f2816eb42a9a6122910119
Andrey Chernenko <xchernen@mendelu.cz>
2025-10-24 19:01:19 +0200

    Time: O(n)

--- src/leetcode/LongestSubstringNoRepeat.java
+++ src/leetcode/LongestSubstringNoRepeat.java
@@ -16,26 +16,13 @@ public class LongestSubstringNoRepeat {
     *
     * */
     public static int lengthOfLongestSubstring(String s) {
-        int left = 0;                // start of the window
-        int max = 0;                 // max substring length found
-        Map<Character, Integer> map = new HashMap<>();  // stores last seen index of each char
-
-        for (int right = 0; right < s.length(); right++) {
-            char c = s.charAt(right);
-
-            // If char was seen before and is inside current window,
-            // move the left pointer right after its last seen position
-            if (map.containsKey(c) && map.get(c) >= left) {
-                left = map.get(c) + 1;
-            }
-
-            // store or update last seen index of this char
-            map.put(c, right);
-
-            // update max length (current window = right - left + 1)
-            max = Math.max(max, right - left + 1);
+        int[] index = new int[128];
+        int max = 0;
+        for (int l = 0, r = 0; r < s.length(); r++) {
+            l = Math.max(index[s.charAt(r)], l);
+            max = Math.max(max, r - l + 1);
+            index[s.charAt(r)] = r + 1;
         }
-
         return max;
     }
 
