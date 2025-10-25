eed0af34fd4e1d90acc1c9283a3d7fdd2ed5b0ff
Andrey Chernenko <xchernen@mendelu.cz>
2025-10-25 09:40:26 +0200

    Time: O(√x) Space: O(1)

---
+++ src/leetcode/SquareRoot.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+/**
+ *
+ * File is created by andreychernenko at 24.10.2025
+ *
+ *
+ */
+
+
+public class SquareRoot {
+
+    // Newton formula
+    public static int mySqrt(int x) {
+        double step = 1;
+        Math.sqrt(1.1);
+        while (Math.round(((step + x/step) / 2) * ((step + x/step) / 2)) != x) {
+            step += 1;
+        }
+        return (int) (step + x/step) / 2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mySqrt(2147395600));
+        //System.out.println(10/100);
+    }
+}
